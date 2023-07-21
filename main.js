fcecfdbf41f6dc7599b819ae085278fa5b34d819
Hawk56767 <76022450+Hawk56767@users.noreply.github.com>
2023-07-21 09:26:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
-var previous_result='';
+img="";
+status="";
+objects=[];
+object_detected=document.getElementById("input");
 function preload(){
+}
+function setup(){
+canvas=createCanvas(380, 380);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(380, 380);
+video.hide();
 
 }
-function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.hide();
-  classifier=ml5.imageClassifier('MobileNet',modelLoaded)
+function Start(){
+    objectdetector=ml5.objectDetector('cocoSSD', modelLoaded);
+    document.getElementById("status").innerHTML="Status: detecting stuff";
 }
 function draw(){
-image(video, 0, 0, 300, 300);
-classifier.classify(video, gotResults);
+image(video, 0, 0, 380, 380);
+if (status!="") {
+for(i=0; i<objects.length; i++){
+    document.getElementById("status").innerHTML="Status: objects are equal to = detected";
+r=random(255);
+g=random(255);
+b=random(255);
+fill(r, g, b);
+confidence_percentage=Math.floor(objects[i].confidence*100);
+text(objects[i].label+ " "+confidence_percentage+ "%",objects[i].x+15,objects[i].y+15);
+noFill();
+stroke(r, g, b);
+rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+}
+objectdetector.detect(video, gotResult);
+document.getElementById("number_of_objects").innerHTML="Number of Objects is equal to = "+objects.length;
+}
 }
 function modelLoaded(){
-console.log("Model Loaded!");
+console.log("Model is Loaded!");
+status=true;
 }
-function gotResults(error, results){
-if (error) {
+function gotResult(error, results){
+if(error){
 console.error(error);
 }
-else{
-if((results[0].confidence>0.5)&&(previous_result != results[0].label)) {
 console.log(results);
-previous_result=results[0].label;
-
-var synth=window.speechSynthesis;
-var speak_data='Object detected is: '+results[0].label;
-var utter_this=new SpeechSynthesisUtterance(speak_data);
-synth.speak(utter_this);
-document.getElementById("result_object").innerHTML=results[0].label;
-document.getElementById("result_accuracy").innerHTML=results[0].confidence.toFixed(3);
+objects=results;
 }
-}
-}
-
-
